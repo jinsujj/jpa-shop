032326e0b4a09559bc936314ac9aa3601dfd879a
jinsu-jj <jinsu.jang@onepredict.com>
2022-11-08 16:29:18 +0900

    repository 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
+
+    @PersistenceContext
     private final EntityManager em;
 
     public void save(Order order) {
@@ -19,5 +23,20 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll() findAll(OrderSearch orderSearch){}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        List<Order> resultList = em.createQuery(
+                "SELECT o " +
+                    "FROM Order o INNER JOIN o.member m " +
+                    "ON o.status = :status " +
+                    "AND m.name LIKE :name",
+                Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+            .setMaxResults(1000) // 최대 1000 건
+            .getResultList();
+
+        return resultList;
+    }
+
+
 }
