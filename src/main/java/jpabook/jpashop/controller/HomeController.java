3bfd9e40d1adae44c5cb3f85292c38fbd1a04bab
jinsu-jj <wlstncjs1234@naver.com>
2022-11-09 19:59:53 +0900

    회원 등록

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,9 +1,9 @@
 package jpabook.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     //Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         log.info("home controller");
         return "home";
     }
+
 
 }
